3537badf594f46dce762c589490def65b5e2206f
Aguibou Barry <aguibou.barry@etu.u-bordeaux.fr>
2022-09-03 01:42:45 +0200

    Liaison du formulaire de contact à la page de contact

--- src/pages/Contact/ContactElements.jsx
+++ src/pages/Contact/ContactElements.jsx
@@ -1,11 +1,12 @@
-import styled from "styled-components";
+import styled, { keyframes } from 'styled-components'
+import colors from '../../utils/style/colors'
 
 /* Mise en forme de la page contact */
 export const TitleStyled = styled.h5`
   font-size: 2rem;
   padding: 15px;
   text-align: center;
-`;
+`
 
 export const CardStyled = styled.div`
   padding: 15px;
@@ -13,24 +14,15 @@ export const CardStyled = styled.div`
   & iframe {
     border-radius: 30% 10%;
   }
-`;
-export const AddressContainer = styled.div`
-  display: flex;
-  gap: 2rem;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-transform: lowercase;
-  font-size: 1.2rem;
-`;
+`
 
 export const ContactContainer = styled.div`
+  margin: 4rem;
+  gap: 1.5rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin: 5rem;
-  margin-bottom: 0;
 
   @media screen and (max-width: 960px) {
     margin-left: 0;
@@ -42,4 +34,125 @@ export const ContactContainer = styled.div`
       font-size: 1.5rem;
     }
   }
-`;
+`
+
+export const ContactContent = styled.div`
+  display: flex;
+  gap: 2rem;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  align-items: center;
+
+  @media screen and (max-width: 960px) {
+    flex-wrap: wrap-reverse;
+  }
+`
+
+export const AddressWrapper = styled.div`
+  display: flex;
+  row-gap: 1rem;
+  flex-direction: column;
+  align-items: center;
+  text-transform: lowercase;
+  font-size: 1rem;
+
+  & span {
+    color: ${({ isDarkMode }) => (isDarkMode ? colors.color16 : colors.color15)};
+  }
+
+  @media screen and (max-width: 440px) {
+    font-size: 0.9rem;
+  }
+`
+const linkAnimate = keyframes`
+  0% { letter-spacing: normal; }
+  50% { letter-spacing: 2px  }
+  100% { letter-spacing: normal; }
+`
+
+export const AddressContent = styled.div`
+  padding: 0.5rem;
+  min-width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 1rem;
+  border: thick double ${colors.special};
+
+  & a {
+    letter-spacing: 2px;
+    animation: ${linkAnimate} 6s ease-in-out infinite 2s;
+  }
+`
+
+export const ContactFormWrapper = styled.div`
+  display: flex;
+  gap: 4rem;
+  flex-direction: column;
+  align-items: center;
+  
+  & h3 {
+    text-align: center;
+    color: ${({ isDarkMode }) => (isDarkMode ? colors.color5 : colors.color6)};
+  }
+
+  & span {
+    color: ${colors.color11};
+  }
+`
+
+const handAnimate = keyframes`
+  0% { margin-top: 1.8rem; font-size: 2.5rem; }
+  50% { margin-top: 2.5rem; font-size: 3rem;  }
+  100% { margin-top: 1.8rem; font-size: 2.5rem; }
+`
+
+export const HandStyled = styled.div`
+  position: absolute;
+  margin-top: 1.8rem;
+  font-size: 2.5rem;
+  color: ${({ isDarkMode }) => (isDarkMode ? colors.color16 : colors.color15)};
+  animation: ${handAnimate} 6s ease-in-out infinite 2s;
+`
+
+export const FormWrapper = styled.form`
+  padding: 1rem;
+  width: 520px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  border-radius: 1rem;
+  border: thick double ${colors.special};
+
+  @media screen and (max-width: 640px) {
+    width: 420px;
+
+    & textarea {
+      width: 270px;
+    }
+  }
+
+  @media screen and (max-width: 440px) {
+    font-size: 0.7rem;
+    width: 350px;
+
+    & textarea, & input {
+      width: 220px;
+    }
+  }
+`
+
+export const FormContent = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: flex-start;
+`
+
+export const FormSubmit = styled.button`
+  padding: 0.5rem;
+  width: max-content;
+  align-self: center;
+`
+
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,38 +1,67 @@
 import { FiMail, FiPhone, FiMapPin } from 'react-icons/fi';
 import { motion } from "framer-motion";
 import {
-  AddressContainer,
+  AddressContent,
+  AddressWrapper,
   // CardStyled,
   ContactContainer,
+  ContactContent,
   TitleStyled,
 } from "./ContactElements";
-
+import ContactForm from '../../components/ContactForm';
+import { FaDiscord, FaWhatsapp } from 'react-icons/fa';
+import { HiArrowRight } from 'react-icons/hi';
+import { useContext } from 'react';
+import { ThemeContext } from '../../utils/context';
 
 function Contact() {
   // const iframeSource = '<iframe title="Mon adresse actuelle" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2831.439985320471!2d-0.5906699848616964!3d44.7922211860499!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5527426d60f443%3A0x8c99a3cb9b8067ee!2s6%20Rue%20Chateaubriand%2C%2033400%20Talence!5e0!3m2!1sfr!2sfr!4v1637447995552!5m2!1sfr!2sfr"> </iframe>'
-  
+  const { theme } = useContext(ThemeContext);
+
   return (
     <motion.div initial={{width: 0}} animate={{width: "100%"}} exit={{width: window.innerWidth, transition: {duration: 0.1}}}>
-      <ContactContainer>
-        <div>
+      <ContactContainer isDarkMode = {theme === "dark"}>
           <TitleStyled>
             Une question? ou autres choses? n'hesitez pas à me contacter !{" "}
           </TitleStyled>
 
-          <AddressContainer>
-            <span>
-              <FiPhone /> 07-51-56-86-53
-            </span>
+          <ContactContent>
+            <AddressWrapper isDarkMode = {theme === "dark"}>
+              <AddressContent>
+                <FiPhone />
+                <h4>Téléphone</h4>
+                <span>07-51-56-86-53</span>
+              </AddressContent>
 
-            <span>
-              <FiMail /> aguiboubarry73@gmail.com
-            </span>
+              <AddressContent>
+                <FiMail />
+                <h4>Email</h4>
+                <span>aguiboubarry73@gmail.com </span>
+                <a href='mailto:aguiboubarry73@gmail.com'>Ecrivez-moi <HiArrowRight /></a>
+              </AddressContent>
 
-            <span>
-              <FiMapPin /> 6 rue chateaubriand, 33400 Talence
-            </span>
-          </AddressContainer>
-        </div>
+              <AddressContent>
+                <FaDiscord />
+                <h4>Discord</h4>
+                <a href='https://discord.com/channels/Agb#2386'>Ecrivez-moi <HiArrowRight /></a>
+              </AddressContent>
+
+              <AddressContent>
+                <FaWhatsapp /> 
+                <h4>Whatsapp</h4>
+                {/* <span>07-51-56-86-53</span> */}
+                <a href='https://api.whatsapp.com/send?phone=0751568653&text=Bonjour !'>Ecrivez-moi <HiArrowRight /></a>
+              </AddressContent>
+
+              <AddressContent>
+                <FiMapPin />
+                <h4>Adresse</h4>
+                <span>6 rue chateaubriand, 33400 Talence</span>
+              </AddressContent>
+            </AddressWrapper>
+
+            <ContactForm />
+          </ContactContent>
 
         {/* <CardStyled dangerouslySetInnerHTML={{__html: iframeSource}} />  */}
       </ContactContainer>
